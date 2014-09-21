e05d607d6421dd3c0cdd9274b7e405fac8bcc99e
francis <francis.yanga@gmial.com>
2014-09-22 11:34:43 +1200

    GA implementation

--- globalErrorTrap.js
+++ globalErrorTrap.js
@@ -1,23 +1,23 @@
-var _gaq = _gaq || [];
 var hasGASetup = false;
 
 function setupGA(GAID){
-  _gaq.push(['_setAccount', GAID]);
-  _gaq.push(['_trackPageview']);
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-  hasGASetup = true;
+	ga('create', GAID, 'auto');
+	ga('send', 'pageview');
+
+	hasGASetup = true;
 }
 
 window.onerror = function(msg, url, line, col, error){
 	var errorMessage = msg + error.stack.split("\n")[1];
 	if (hasGASetup){
-		_gaq.push(['Error', 'javascript', 'Advanced Forms', errorMessage]);
-		console.log(msg + error.stack.split("\n")[1]);
+		//_gaq.push(['Error', 'javascript', 'Advanced Forms', errorMessage]);
+		ga('send', 'event', 'category', 'JSError', errorMessage);
+		console.log(errorMessage);
 		console.log('Error sent to GA');
 	}
 	else{
